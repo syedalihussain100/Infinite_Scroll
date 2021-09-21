f9c3f16e1b234dd543a8915504dfadcd2fab54b4
syedalihussain100 <smuheminali@gmail.com>
2021-09-21 04:41:20 -0700

    changes

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ let Container = document.querySelector('.container');
 let limit = 4;
 let pageCount = 1;
 let postCount = 1;
+
+const getPost = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${pageCount}`);
+    const data = await response.json();
+    console.log(data);
+
+    data.map((currElm, index) => {
+        const htmldata = `
+        <div class="posts">
+        <p class="post_id">${postCount++}</p>
+        <h2 class="title">${currElm.title}</h2>
+        <p class="description">${currElm.body}</p>
+    </div>
+      `
+      Container.insertAdjacentHTML('beforeend',htmldata)
+    })
+}
+
+getPost()
